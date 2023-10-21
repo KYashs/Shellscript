dce79a092e0bccc1c36012abf6d5d2d876ecb091
KYash <134302671+KYashs@users.noreply.github.com>
2023-10-21 19:21:56 +0530

    Update and rename backup.sh to 3backup.sh

--- 3backup.sh
+++ 3backup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 #Author: KYash
+#set -x #debug code
+#set -e #exit script when error terminate further compilation
+#set -o #-e with pipefail
+
 
 #Creating Backup with Crontab
-
 src=~/Documents/devops/1Linux/BashScript
 tgt=~/Documents/backup
+
+#Naming file
 file_name=backup$(date | xargs | awk '{print $2"-"$3"-"$4 }')
 
 #Compressing files
 tar -czvf $tgt/$file_name.tar.gz $src
+
 
 echo "Backup Completed."
